247f8338345fc1a0ffc4ba2af6af31b9c1ab6763
Santiago Spinetto Jung <sanjs965@gmail.com>
2019-05-06 06:41:09 -0300

    CRUD

---
+++ CRUD-PDO/public/index.php
@@ -0,0 +1,10 @@
+<?php include "templates/header.php"; ?>
+
+<ul>
+	<li><a href="create.php"><strong>Crear</strong></a> - add a user</li>
+	<li><a href="read.php"><strong>Leer</strong></a> - find a user</li>
+	<li><a href="update.php"><strong>Actualizar</strong></a> - edit a user</li>
+	<li><a href="delete.php"><strong>Borrar</strong></a> - delete a user</li>
+</ul>
+
+<?php include "templates/footer.php"; ?>
